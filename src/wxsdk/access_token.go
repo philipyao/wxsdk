8defeb9aa8fed48de3515750126002f41ee016ad
philipyao <187239625@qq.com>
2017-08-24 11:57:44 +0800

    convert tab

--- src/wxsdk/access_token.go
+++ src/wxsdk/access_token.go
@@ -1,30 +1,30 @@
 package wxsdk
 
 import(
-	"fmt"
+    "fmt"
     "sync"
-	"time"
+    "time"
 )
 
 const (
-	//APPID           = "wxa2f6ba782c07b339"
-	//APPSecret       = "d49da064fcca4c57d191f3484d6f9748"
+    //APPID           = "wxa2f6ba782c07b339"
+    //APPSecret       = "d49da064fcca4c57d191f3484d6f9748"
 
-	APPID           = "wx4d953a6ad5112405"
-	APPSecret       = "56657d6773e91ee3a2814273b9813fc0"
-	UrlAccessToken  = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%v&secret=%v"
+    APPID           = "wx4d953a6ad5112405"
+    APPSecret       = "56657d6773e91ee3a2814273b9813fc0"
+    UrlAccessToken  = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%v&secret=%v"
 )
 
 type accessToken struct{
-	token   string
-	sync.RWMutex
+    token   string
+    sync.RWMutex
 }
 
 type TokenRsp struct {
-	WeiXinRspHeader
+    WeiXinRspHeader
 
-	Token   string              `json:"access_token"`
-	ExpireIn int                `json:"expires_in"`
+    Token   string              `json:"access_token"`
+    ExpireIn int                `json:"expires_in"`
 }
 
 //获取AccessToken
@@ -32,43 +32,43 @@ var AccessToken func() string
 
 //维护AccessToken
 func keepAccessToken() {
-	var _token = new(accessToken)
+    var _token = new(accessToken)
 
     AccessToken = func() string {
-	    _token.RLock()
+        _token.RLock()
         defer _token.RUnlock()
         return _token.token
     }
 
     // for 循环刷新
-	go func() {
-		for {
-			token, expirein, err := requestAccessToken()
-			if err != nil {
-				fmt.Printf("requestAccessToken error %v\n", err)
-				time.Sleep(time.Minute)
-				continue
-			}
-			fmt.Printf("requestAccessToken success: expire %v, token %v\n", expirein, token)
-			_token.Lock()
-			_token.token = token
-			_token.Unlock()
+    go func() {
+        for {
+            token, expirein, err := requestAccessToken()
+            if err != nil {
+                fmt.Printf("requestAccessToken error %v\n", err)
+                time.Sleep(time.Minute)
+                continue
+            }
+            fmt.Printf("requestAccessToken success: expire %v, token %v\n", expirein, token)
+            _token.Lock()
+            _token.token = token
+            _token.Unlock()
 
-			timer := time.NewTimer(time.Second * time.Duration(expirein))
-			<- timer.C
-		}
-	}()
+            timer := time.NewTimer(time.Second * time.Duration(expirein))
+            <- timer.C
+        }
+    }()
 }
 
 func requestAccessToken() (string, int, error) {
-	url := fmt.Sprintf(UrlAccessToken, APPID, APPSecret)
-	var rsp TokenRsp
-	err := getJson(url, &rsp)
-	if err != nil {
-		return "", 0, err
-	}
-	if rsp.ErrorCode != 0 {
-		return "", 0, fmt.Errorf("requestAccessToken error: %v %v", rsp.ErrorCode, rsp.ErrorMessage)
-	}
-	return rsp.Token, rsp.ExpireIn, nil
+    url := fmt.Sprintf(UrlAccessToken, APPID, APPSecret)
+    var rsp TokenRsp
+    err := getJson(url, &rsp)
+    if err != nil {
+        return "", 0, err
+    }
+    if rsp.ErrorCode != 0 {
+        return "", 0, fmt.Errorf("requestAccessToken error: %v %v", rsp.ErrorCode, rsp.ErrorMessage)
+    }
+    return rsp.Token, rsp.ExpireIn, nil
 }
